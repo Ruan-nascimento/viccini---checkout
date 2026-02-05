2208c07aeedcc871696c7278df0d162230deb4b1
PlayRunOffc <playruns3@gmail.com>
2026-02-05 20:47:35 -0300

    feat: Add PIX payment processing with new API endpoints for creating and checking PIX QR codes.

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createCustomer, getCustomers } from './customer';
-import { createBilling } from './billing';
+import { createBilling, getBilling } from './billing';
+import { checkPix, createPix } from './pix';
 
 const router = Router();
 
@@ -12,5 +13,14 @@ router.get('/get-customers', getCustomers);
 
 // POST /api/create-billing
 router.post('/create-billing', createBilling);
+
+// GET /api/get-billing
+router.get('/get-billing', getBilling);
+
+// POST /api/create-pix
+router.post('/create-pix', createPix);
+
+// GET /api/check-pix/:id
+router.get('/check-pix/:id', checkPix);
 
 export default router;
